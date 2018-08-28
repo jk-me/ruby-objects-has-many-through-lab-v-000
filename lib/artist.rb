342a73f3ced35493df5831eb8c367465327e4bbf
Jenny K <jkam17@gmail.com>
2018-08-28 22:17:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,10 +10,10 @@ class Artist
   end 
   def new_song(name,genre)
     n=Song.new(name,self,genre)
-    n.artist=self 
+    #n.artist=self 
   end 
   def songs 
-    Songs.all.select {|s| s.artist==self}
+    Song.all.select {|s| s.artist==self}
   end 
   def genres 
     self.songs.collect{|s| s.genre}
