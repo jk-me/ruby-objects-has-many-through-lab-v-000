e253331a2a98b14c692c1748809b6cab8218799c
Jenny K <jkam17@gmail.com>
2018-08-28 22:34:34 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,28 +6,15 @@ class Doctor
     @name=name 
     @@all<< self
     @appointments=[]
+    @patients=[]
   end 
   def new_appointment(patient, date)
-    a=Appointment.new(patient,date)
+    a=Appointment.new(patient,self,date)
     @appointments << a 
+    @patients << patient 
   end
     
-
 end 
-  describe "#name" do
-    it "has a name" do
-      doctor_who = Doctor.new("The Doctor")
-      expect(doctor_who.name).to eq("The Doctor")
-    end
-  end
-
-  describe "#new" do
-    it "initializes with a name and adds itself to an array of all doctors" do
-      doctor_smith = Doctor.new("John Smith")
-      expect{ Doctor.new("Martha Jones")}.to_not raise_error
-      expect(Doctor.all).to include(doctor_smith)
-    end
-  end
 
   describe "#new_appointment" do
     it "given a date and a patient, creates a new appointment" do
