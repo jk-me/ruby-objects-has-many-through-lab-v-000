4975b8c4a49db4a5bb0e70be9cf8b9a0ee58a3ec
Jenny K <jkam17@gmail.com>
2018-08-28 22:16:10 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,7 +10,7 @@ class Artist
   end 
   def new_song(name,genre)
     n=Song.new(name,self,genre)
-    #n.artist=self 
+    n.artist=self 
   end 
   def songs 
     @@all.select {|s| s.artist==self}
