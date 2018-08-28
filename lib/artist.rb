0c1e0ed460d10a4ecd404848552daba333c76b53
Jenny K <jkam17@gmail.com>
2018-08-28 22:06:45 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,24 +9,17 @@ class Artist
   def self.all 
     @@all 
   end 
-  
   def new_song(name)
     n=Song.new(name)
     n.artist=self 
   end 
-  
   def songs 
     @@all.select {|s| s.artist==self}
   end 
-  
   def genres 
     self.songs.collect{|s| s.genre}
   end
-
 end 
-
-require "spec_helper"
-
 
 describe "Artist" do
   describe "#name" do
